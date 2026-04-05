5000f4e80398e9e88694383a6b33462ea6338770
irsw822 <irsw822@gmail.com>
2026-04-06 07:53:10 +0900

    Update juggler.js

--- juggler.js
+++ juggler.js
@@ -1,6 +1,11 @@
 let status = "NONE";
 let result = "NONE";
-let audio_gako = new Audio('mp3/dageki6.mp3');
+let audio_gako = new Audio('mp3/gako.mp3');
+let audio_bet = new Audio('mp3/bet.mp3');
+let audio_start = new Audio('mp3/start.mp3');
+let audio_stop = new Audio('mp3/stop.mp3');
+let audio_replay = new Audio('mp3/replay.mp3');
+let audio_budo = new Audio('mp3/budo.mp3');
 
 function setResult() {
 	result = "NONE";
@@ -20,10 +25,37 @@ function setResult() {
 
 
 document.addEventListener('DOMContentLoaded', function () {
+	audio_bet.preload = 'auto';
+	audio_bet.addEventListener('canplaythrough', () => {
+		console.log('音声ファイルのプリロードが完了しました');
+	});
+
+	audio_start.preload = 'auto';
+	audio_start.addEventListener('canplaythrough', () => {
+		console.log('音声ファイルのプリロードが完了しました');
+	});
+
+	audio_stop.preload = 'auto';
+	audio_stop.addEventListener('canplaythrough', () => {
+		console.log('音声ファイルのプリロードが完了しました');
+	});
+	
 	audio_gako.preload = 'auto';
 	audio_gako.addEventListener('canplaythrough', () => {
 		console.log('音声ファイルのプリロードが完了しました');
 	});
+
+
+	audio_budo.preload = 'auto';
+	audio_budo.addEventListener('canplaythrough', () => {
+		console.log('音声ファイルのプリロードが完了しました');
+	});
+
+	audio_replay.preload = 'auto';
+	audio_replay.addEventListener('canplaythrough', () => {
+		console.log('音声ファイルのプリロードが完了しました');
+	});
+
 	
 	btn = document.getElementById('id_button');
 	txt = document.getElementById('id_text');
@@ -31,19 +63,36 @@ document.addEventListener('DOMContentLoaded', function () {
 	btn.addEventListener('pointerdown', () => {
 		btn.classList.add('pressed');
 		if (status === "BET") {
+			audio_start.play();
 			status = "STARTED"
 			console.log("スタートします");
 			setResult();
 		} else if (status === "STARTED") {
+			audio_stop.play();
 			status = "PUSHED1"
 			console.log("ボタン1をpushしました");
 		} else if (status === "PUSHED1") {
+			audio_stop.play();
 			status = "PUSHED2"
 			console.log("ボタン2をpushしました");
 		} else if (status === "PUSHED2") {
+			audio_stop.play();
 			status = "PUSHED3"
 			console.log("ボタン3をpushしました");
+
+			if (result === "BUDO") {
+				//console.log("ぶどうget");
+				audio_budo.play();
+				alert("ぶどうget");
+			} else if (result === "REPLAY") {
+				//console.log("リプレイget");    
+				audio_replay.play();
+				alert("リプレイget");
+			}
+
+			
 		} else {
+			audio_bet.play();
 			status = "BET"
 			console.log("BETしました");
 		}
@@ -57,12 +106,6 @@ document.addEventListener('DOMContentLoaded', function () {
 				//console.log("ガコッ！");  
 				audio_gako.play();
 				alert("ガコッ！");
-			} else if (result === "BUDO") {
-				//console.log("ぶどうget");    
-				alert("ぶどうget");
-			} else if (result === "REPLAY") {
-				//console.log("リプレイget");    
-				alert("リプレイget");
 			}
 		}
 	});
