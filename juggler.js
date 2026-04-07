021852a988195c788de6d5e712aa87e6a7def80e
irsw822 <irsw822@gmail.com>
2026-04-07 14:20:01 +0900

    Update juggler.js

--- juggler.js
+++ juggler.js
@@ -114,6 +114,7 @@ document.addEventListener('DOMContentLoaded', async function () {
           // AudioContextはユーザー操作があるまでサスペンドされていることがあるのでresumeする
     if (audioContext.state === 'suspended') {
       await audioContext.resume();
+      await new Promise(resolve => setTimeout(resolve, 100)); // 100ms待つ
       console.log('AudioContext resumed');
     }
     console.log(audioContext.state);
